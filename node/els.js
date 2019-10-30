72f5a5668aaa89ea12df27d32af3632b772851f3
nudelx <nudelx@gmail.com>
2019-10-30 14:53:37 +0200

    done

--- node/els.js
+++ node/els.js
@@ -1,7 +1,14 @@
 const elasticsearch = require('elasticsearch')
 
+class ElsConnectionPool extends elasticsearch.ConnectionPool {
+  markAlive(connection) {
+    super.markAlive(connection)
+  }
+}
+
 const els = {
   els: new elasticsearch.Client({
+    ConnectionPool: ElsConnectionPool,
     host: 'localhost:9200',
     log: 'trace',
     apiVersion: '5.6'
